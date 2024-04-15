fdf4fb3141b22a91f716d9aa82607c1c8e4ed8f7
Omprakash Rawat <omprakash824rawat@gmail.com>
2024-04-15 16:58:47 +0530

    Passenger added

--- Lecture 4/airline/flights/views.py
+++ Lecture 4/airline/flights/views.py
@@ -7,3 +7,11 @@ def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
     })
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(id=flight_id)
+    passengers = flight.passengers.all()
+    return render(request, "flights/flight.html", {
+        "flight": flight,
+        "passengers": passengers
+    })
