5f611b0eff6a3d84df5c7dc9cbc1c41509b0ed0b
Omprakash Rawat <omprakash824rawat@gmail.com>
2024-04-19 14:52:40 +0530

    Random Books is Working

--- Lecture 4/Booky/books/views.py
+++ Lecture 4/Booky/books/views.py
@@ -1,3 +1,12 @@
+import random
 from django.shortcuts import render
+from .models import *
+
+
 
 # Create your views here.
+def index(request):
+    random_books = Book.objects.filter(pk__in=random.sample(range(Book.objects.count()), 6))
+    return render(request, "books/index.html", {
+        "random_books" : random_books
+    }) 
