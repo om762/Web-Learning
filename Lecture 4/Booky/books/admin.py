e64b95d75106da50e83f780b9be907157bbe6463
Omprakash Rawat <omprakash824rawat@gmail.com>
2024-04-23 10:00:14 +0530

    Booky User (Not Completed for now)

--- Lecture 4/Booky/books/admin.py
+++ Lecture 4/Booky/books/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Book, BookReader, Person
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(BookReader)
+admin.site.register(Person)
