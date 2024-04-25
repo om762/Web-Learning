c9001cb52dd244ba1a975520852f8e37f0e94b72
Omprakash Rawat <omprakash824rawat@gmail.com>
2024-04-25 09:54:16 +0530

    Project Name: Commerce state: Registaion and login is Working status �

--- Projects/commerce/auctions/models.py
+++ Projects/commerce/auctions/models.py
@@ -3,4 +3,19 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    id = models.BigAutoField(primary_key=True)
+
+
+class Listing(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    item_name = models.CharField(max_length=100)
+    description = models.CharField(max_length=5000)
+    starting_bid = models.IntegerField()
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name="items")
+    
+    def __str__(self) -> str:
+        return self.item_name
+
+class Category(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    type = models.CharField(max_length=35)
