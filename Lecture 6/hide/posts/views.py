24baffbaa461fcb0eed8d1096453c65469efbba7
om762 <omprakash824rawat@gmail.com>
2024-11-11 11:05:45 +0530

    Hide Complete

---
+++ Lecture 6/hide/posts/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from time import sleep
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def posts(request):
+    start = int(request.GET.get("start") or 1)
+    end = int(request.GET.get("end") or 10)
+    
+    sleep(2)
+    
+    new_posts = []
+    for i in range(start, end + 1):
+        new_posts.append(f"Post #{i}")
+    
+    return JsonResponse({
+        "posts":new_posts
+    })
